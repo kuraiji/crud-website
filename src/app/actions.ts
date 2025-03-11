e287d9b66aff94475acf6636dfdd6779b38f39e8
Payman <crygie@hotmail.com>
2025-03-10 17:41:55 -0700

    Added authorizer to AWS API Gateway and connected server action to it

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,11 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import {revalidatePath} from 'next/cache'
+import {redirect} from 'next/navigation'
+import {createClient} from '@/utils/supabase/server'
 
-import { createClient } from '@/utils/supabase/server'
 
+//Supabase
 export async function signout() {
     const supabase = await createClient()
 
@@ -18,4 +19,39 @@ export async function signout() {
 
     revalidatePath('/', 'layout')
     redirect('/')
+}
+
+
+//DynamoDB
+type DBUser = {
+    id: string,
+    role: string,
+}
+
+export async function getDBUser() {
+    const supabase = await createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+        try {
+            return await fetch(`${process.env.NEXT_PUBLIC_CRUD_URL}/user/${user.id}`,
+                {
+                    method: 'GET',
+                    cache: 'no-store',
+                    headers: {
+                        "authorizationToken": `${process.env.NEXT_PUBLIC_CRUD_ANON_KEY}`,
+                    }
+                }).then(async function (res) {
+                const data: DBUser = await res.json();
+                return data;
+            });
+        }
+        catch (e) {
+            console.log("Error: ", e);
+        }
+    }
+    return null;
 }
